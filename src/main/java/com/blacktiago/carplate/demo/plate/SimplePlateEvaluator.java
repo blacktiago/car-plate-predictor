a7a6171b1b64de58fd3db7dacb1105396670cadd
blacktiago <santiago.plascencia@modinter.com.ec>
2020-02-14 10:49:00 -0500

    feature: plate test on green https://github.com/users/blacktiago/projects/1#card-33134142

--- src/main/java/com/blacktiago/carplate/demo/plate/SimplePlateEvaluator.java
+++ src/main/java/com/blacktiago/carplate/demo/plate/SimplePlateEvaluator.java
@@ -1,18 +1,24 @@
 package com.blacktiago.carplate.demo.plate;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class SimplePlateEvaluator implements PlateEvaluator{
+
+    private Pattern pattern = Pattern.compile("[A-Za-z]{3} [0-9]{3,4}");
 
     @Override
     public boolean isValidPlate(String plate) {
 
+        plate = plate.trim();
+        Matcher matcher = pattern.matcher(plate);
 
-        return false;
+        return plate.length() > 5 && plate.length() < 9 && matcher.matches();
     }
 
     @Override
     public int getEvaluationDigit(String plate) {
 
-        return 2;
+        return Integer.parseInt(plate.substring(plate.length() - 1));
     }
 }
