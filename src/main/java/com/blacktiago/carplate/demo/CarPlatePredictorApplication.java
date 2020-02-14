28146d25ee19dd2668726b17f5873b81b5ca6af7
blacktiago <santiago.plascencia@modinter.com.ec>
2020-02-13 20:22:42 -0500

    initial commit

---
+++ src/main/java/com/blacktiago/carplate/demo/CarPlatePredictorApplication.java
@@ -0,0 +1,13 @@
+package com.blacktiago.carplate.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarPlatePredictorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CarPlatePredictorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/blacktiago/carplate/demo/CarPlatePredictorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blacktiago.carplate.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarPlatePredictorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
