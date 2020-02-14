3639034578afac42c40d9b475ad92c2816c1d784
blacktiago <santiago.plascencia@modinter.com.ec>
2020-02-14 11:37:40 -0500

    feature: time test on green https://github.com/users/blacktiago/projects/1#card-33134142

--- src/main/java/com/blacktiago/carplate/demo/time/SimpleTimeEvaluator.java
+++ src/main/java/com/blacktiago/carplate/demo/time/SimpleTimeEvaluator.java
@@ -1,10 +1,30 @@
 package com.blacktiago.carplate.demo.time;
 
+import java.util.Optional;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class SimpleTimeEvaluator implements TimeEvaluator{
+
+    private Pattern pattern = Pattern.compile("[0-9]{2}:[0-9]{2}");
 
     @Override
     public boolean isValidTime(String time) {
-        return false;
+        boolean isValid = false;
+        if(Optional.ofNullable(time).isPresent()){
+            time = time.trim();
+            Matcher matcher = pattern.matcher(time);
+
+            isValid = matcher.matches();
+        }
+
+        if(isValid){
+            String[] timeParts = time.split(":");
+            int hour = Integer.parseInt(timeParts[0]);
+            int minute = Integer.parseInt(timeParts[1]);
+            isValid = (hour >= 0 && hour <= 23) && (minute >= 0 && minute <= 59);
+        }
+        return isValid;
     }
 
 }
--- src/test/java/com/blacktiago/carplate/demo/TimeValueTest.java
+++ src/test/java/com/blacktiago/carplate/demo/TimeValueTest.java
@@ -9,15 +9,24 @@ import org.springframework.util.Assert;
 @SpringBootTest
 class TimeValueTest {
 
-    TimeEvaluator timeEvaluator = new SimpleTimeEvaluator();
+    private TimeEvaluator timeEvaluator = new SimpleTimeEvaluator();
 
     @Test
     void validFormat(){
-        Assert.isTrue(timeEvaluator.isValidTime("03"), "03 am test");
-        Assert.isTrue(timeEvaluator.isValidTime("5"), "5 am test");
-        Assert.isTrue(timeEvaluator.isValidTime("12"), "mid day");
-        Assert.isTrue(timeEvaluator.isValidTime("16"), "16 pm");
-        Assert.isTrue(timeEvaluator.isValidTime("20"), "20 pm");
-        Assert.isTrue(timeEvaluator.isValidTime("00"), "mid night");
+        Assert.isTrue(timeEvaluator.isValidTime("00:00"), "mid night");
+        Assert.isTrue(timeEvaluator.isValidTime("03:15"), "03 am test");
+        Assert.isTrue(timeEvaluator.isValidTime("05:30"), "5 am test");
+        Assert.isTrue(timeEvaluator.isValidTime("12:00"), "mid day");
+        Assert.isTrue(timeEvaluator.isValidTime("16:20"), "16 pm");
+        Assert.isTrue(timeEvaluator.isValidTime("20:45"), "20 pm");
+    }
+
+    @Test
+    void invalidFormat(){
+        Assert.isTrue(!timeEvaluator.isValidTime("sdfd"), "letters");
+        Assert.isTrue(!timeEvaluator.isValidTime("200"), "just one number");
+        Assert.isTrue(!timeEvaluator.isValidTime("-1"), "negative");
+        Assert.isTrue(!timeEvaluator.isValidTime("24:00"), "hour longer than 23");
+        Assert.isTrue(!timeEvaluator.isValidTime("12:60"), "minute longer than 59");
     }
 }
